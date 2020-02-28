b1e98b51cfa814fcec6abf810d8d4c772245b570
lromeral <luis.romeral@gmail.com>
2020-02-28 21:37:46 +0100

    Mat-Tables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import {TableModule} from 'primeng/table';
 import { AppComponent } from './app.component';
 import { CountriesmoduleComponent } from './countriesmodule/countriesmodule.component';
 import { PersonasComponent } from './personas/personas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +20,10 @@ import { PersonasComponent } from './personas/personas.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    TableModule
+    TableModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { PersonasService } from '../services/personas.service';
 
+
+
+
+
+
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
@@ -8,24 +13,54 @@ import { PersonasService } from '../services/personas.service';
 })
 export class PersonasComponent implements OnInit {
 
-  constructor( private personasService: PersonasService) { }
+  constructor(private personasService: PersonasService) { }
 
+
+  cols:any[];
+  displayedColumns: string[]=['avatar','nombre'];
+  dataSource: string;
   personas: any[]=[];
+  otroArray: any[]=[];
 
   ngOnInit(): void {
 
+    this.cols =[
+      {nombre: 'avatar', display:"Avatar", width:'10%'},
+      {nombre: 'nombre', display:"Nombre"},
+      {nombre: 'apellido', display:"Apellido"},
+      {nombre: 'email', display:"E-mail"},
+      {nombre: 'ciudad', display:"Ciudad"},
+      {nombre: 'sueldo', display:"Sueldo"}
+
+    ]
+    this.getPersonas()
+    
+    this.prueba();
+    console.log(this.otroArray);
+  
+  }
+
+  getPersonas() {
     this.personasService.getPersonas().subscribe(
       data => {
-        data.forEach(element =>{
-          this.personas.push(element);
-        } )
+        this.personas = data
+        //console.log(this.personas);
       },
       err => console.log(err),
       () => console.log("okey")
-    );
-
-    console.log(this.personas);
+    ).unsubscribe;
   }
 
+  prueba(){
+    var miArray: any[]=[];
+    for (let i:number=0; i < 10; i++){
+      miArray.push ([{id: i, valor: i}]);
+    }
+    
+    this.otroArray = miArray;
+    //console.log(miArray);
+    console.log("prueba");
+    
+  }
 
 }
